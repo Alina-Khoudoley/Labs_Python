27199ba1abb3d854e32b1323c0cd56981055a768
Alina <alinakhoudoley123@gmail.com>
2021-02-09 18:01:32 +0200

    Labs 1-2 are done

---
+++ lab01-02/src/main.py
@@ -0,0 +1,31 @@
+import math
+
+
+def data(s):
+    loop = True
+    while loop:
+        try:
+            v = float(input(s))
+            loop = type(v) != float or v < 0
+            if v < 0:
+                raise Exception("Ви ввели неправильне число.")
+        except:
+            print("Ви ввели неправильне число.")
+    return v
+
+
+x = data("Введіть додатне число x(ціле або дробове з крапкою): ")
+y = data("Введіть додатне число y(ціле або дробове з крапкою): ")
+
+z = (math.pow(math.cos(math.radians(x)), 2) + math.pow(math.sin(math.radians(y)), 2) +
+     (1 / 4) * math.pow(math.sin(math.radians(2 * x)), 2) - 1)
+print("Результат =", z)
+
+scholarship = data("Введіть стипендію: ")
+costs_per_month = data("Введіть витрати: ")
+while costs_per_month < scholarship:
+    print("Ви ввели неправильне число.")
+    costs_per_month = data("Введіть витрати: ")
+
+total_costs = costs_per_month * (1.05 ** 10 - 1) / 0.05
+print(f'Позика = {math.ceil(total_costs - scholarship * 10)}')
