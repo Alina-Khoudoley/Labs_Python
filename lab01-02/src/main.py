2ca52ea1c8efae6454b014f9d82b604a6a177425
Alina <alinakhoudoley123@gmail.com>
2021-02-10 18:16:55 +0200

    Lab 3 is done

--- lab01-02/src/main.py
+++ lab01-02/src/main.py
@@ -21,11 +21,14 @@ z = (math.pow(math.cos(math.radians(x)), 2) + math.pow(math.sin(math.radians(y))
      (1 / 4) * math.pow(math.sin(math.radians(2 * x)), 2) - 1)
 print("Результат =", z)
 
+proc = 105
+month = 10
 scholarship = data("Введіть стипендію: ")
 costs_per_month = data("Введіть витрати: ")
 while costs_per_month < scholarship:
     print("Ви ввели неправильне число.")
     costs_per_month = data("Введіть витрати: ")
 
-total_costs = costs_per_month * (1.05 ** 10 - 1) / 0.05
+# total_costs = costs_per_month * (1.05 ** 10 - 1) / 0.05
+total_costs = costs_per_month * ((proc * (10 ** -2)) ** month - 1) / (proc * (10 ** -2) - 1)
 print(f'Позика = {math.ceil(total_costs - scholarship * 10)}')
---
+++ lab03/src/main.py
@@ -0,0 +1,118 @@
+import random
+
+
+def data(s):
+    loop = True
+    while loop:
+        try:
+            v = int(input(s))
+            loop = type(v) != int or v < 0
+            if v < 0:
+                raise Exception("Ви ввели неправильне число.")
+        except:
+            print("Ви ввели неправильне число.")
+    return v
+
+
+def prod(nl):
+    i = 0
+    p = 1
+    while i < len(nl[1]):
+        if nl[1][i] < 0:
+            p *= nl[1][i]
+        i += 1
+    return p
+
+
+def my_count(nl):
+    i = 0
+    c = 0
+    while i < len(nl):
+        if nl[i][1] % 5 != 0:
+            c += 1
+        i += 1
+    return c
+
+
+def main():
+    n = data("Введіть кількість елементів у списку: ")
+    my_list = list()
+    i = 0
+    while i < n:
+        my_list.append(random.randint(0, 50))
+        i += 1
+    print(my_list)
+
+    j = 0
+    k = data("Введіть на скільки позицій вліво будуть циклічно зсунуті усі елементи списку: ")
+    new_list = list(my_list)
+    i = 0
+    while i < len(my_list) - k:
+        new_list[i] = my_list[i + k]
+        i += 1
+
+    while len(my_list) - k <= i < len(new_list):
+        new_list[i] = my_list[j]
+        i += 1
+        j += 1
+    print(new_list)
+
+    nested_list = list()
+    n = data("Введіть кількість рядків(мінімум 2): ")
+    if n < 2:
+        print("Ви ввели неправильне число.")
+        n = data("Введіть кількість рядків(мінімум 2): ")
+    m = data("Введіть кількість стовпців(мінімум 2): ")
+    if m < 2:
+        print("Ви ввели неправильне число.")
+    i = 0
+    j = 0
+    while i < n:
+        nested_list_s = list()
+        while j < m:
+            nested_list_s.append(random.randint(-10, 10))
+            j += 1
+        j = 0
+        nested_list.append(nested_list_s)
+        i += 1
+    for nl in nested_list:
+        for item in nl:
+            print(item, end=" | ")
+        print('\n')
+
+    product = prod(nested_list)
+    print(f'Добуток від’ємних елементів другого рядка: = {product}')
+    c = my_count(nested_list)
+    print(f'Кількість елементів в другому стовпчику, які не кратні «5»: = {c}')
+
+    sent1 = "Hello, world world. "
+    sent2 = "Hello this is Alina. "
+    sent1 = sent1.strip()
+    sent1 = sent1.replace("-", " ")
+    sent1 = sent1.replace(",", " ")
+    sent1 = sent1.replace(".", " ")
+    sent2 = sent2.strip()
+    sent2 = sent2.replace("-", " ")
+    sent2 = sent2.replace(",", " ")
+    sent2 = sent2.replace(".", " ")
+
+    splitted_sent1 = sent1.split()
+    splitted_sent2 = sent2.split()
+    splitted_list = splitted_sent1 + splitted_sent2
+
+    print("Слова, що зустрічаються один раз в обох реченнях: ")
+    i = 0
+    my_list = list()
+    while i < len(splitted_list):
+        c = splitted_list.count(splitted_list[i])
+        if c == 1:
+            my_list.append(splitted_list[i])
+        i += 1
+    if not my_list:
+        print("Такі слова не знайдені.")
+    else:
+        print(my_list)
+
+
+if __name__ == "__main__":
+    main()
