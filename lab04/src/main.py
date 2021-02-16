f83134fa9b76f7261b43a931840a6bec4e588ace
Alina <alinakhoudoley123@gmail.com>
2021-02-16 19:13:43 +0200

    Report for fourth lab is done

--- lab04/src/main.py
+++ lab04/src/main.py
@@ -3,7 +3,25 @@ import random
 
 def my_print(magazines):
     for key in magazines:
-        print("Magazine #", key, " - ", magazines[key])
+        print("Magazine \"", key, "\" - ", magazines[key])
+
+
+def av_price(sort_list, magazines):
+    total_price = 0
+    i = 0
+    c = 0
+    print("Average price of magazines whose circulation is less than 10 000:")
+    for mag in sort_list:
+        key = sort_list[i], "circulation"
+        key1 = sort_list[i], "price"
+        if magazines[key[0]][key[1]] < 10000:
+            total_price += magazines[key1[0]][key1[1]]
+            c += 1
+        i += 1
+    if c == 0: print("No magazine with circulation less than 10000 was found.")
+    else:
+        average_price = total_price / c
+        print(average_price)
 
 
 def main():
@@ -36,17 +54,17 @@ def main():
     magazines["The Guardian"]["price"] = random.randint(20, 60)
     magazines["The Guardian"]["circulation"] = random.randrange(7000, 15000, 100)
 
-    print("New data: ")
+    print("New data after adding one element: ")
     my_print(magazines)
 
     del magazines["The Guardian"]
 
-    print("New data: ")
+    print("New data after deleting one element: ")
     my_print(magazines)
 
     sl = list()
     i = 0
-    print("Sorted data: ")
+    print("Sorted data by alphabet: ")
     for key in magazines:
         sl.append(key)
     sort_list = list(sl)
@@ -55,18 +73,7 @@ def main():
     for item in sort_list:
         print(item, "-", magazines[item])
 
-    total_price = 0
-    c = 0
-    print("Average price of magazines whose circulation is less than 10 000:")
-    for mag in sort_list:
-        key = sort_list[i], "circulation"
-        key1 = sort_list[i], "price"
-        if magazines[key[0]][key[1]] < 10000:
-            total_price += magazines[key1[0]][key1[1]]
-            c += 1
-        i += 1
-    av_price = total_price/c
-    print(av_price)
+    av_price(sort_list, magazines)
 
 
 if __name__ == "__main__":
